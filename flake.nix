56b2e2786b362b29e5611ce2ef02ad677ff81daa
Morgan Hamill <chamill@grailbio.com>
2021-06-22 13:06:38 -0700

    Add Nix package derivation
    * Update Debian package to use Makefile.
    * Update copyright information.
    * Bump version.

---
+++ flake.nix
@@ -0,0 +1,22 @@
+{
+  description = "mo-utils utility collection.";
+
+  inputs = {
+    nixpkgs.url = "nixpkgs/nixos-21.05";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, flake-utils, nixpkgs }: {
+    overlay = final: prev: {
+      mo-utils = prev.callPackage ./. { };
+    };
+  } // flake-utils.lib.eachDefaultSystem (system:
+    let pkgs = import nixpkgs {
+      inherit system;
+      overlays = [ self.overlay ];
+    }; in {
+      defaultPackage = pkgs.mo-utils;
+      packages = { inherit (pkgs) mo-utils; };
+    }
+  );
+}
